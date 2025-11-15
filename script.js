acc882cc32f02c5a9e6d01c8b7a220f408415dcd
Adiba Haque Malick <154227426+adibamallick@users.noreply.github.com>
2025-11-15 11:53:12 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+/*
+document.getElementById('title').innerHTML="HELLO  WORLD";
+let sum=50+40;
+console.log(sum);
+if (sum>100){
+    console.log('expensive');
+
+}
+else{
+    console.log('cheap');
+}
+
+function sayHello(name){
+    console.log("Hello " + name);
+}
+   sayHello('Adiba');*/
+
+var a=10;
+var a=20;
+a='bangladesh';
+
+console.log(a);
+
+let firstName = 'Adiba';
+console.log(`${firstName} is my name, ${firstName} is a girl`);
